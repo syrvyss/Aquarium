746d6db3200123ea45734fa1f472ba2078523f11
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-24 12:41:01 +0200

    Program init

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aquarium {
+    class Program {
+        public static void Main() {
+            
+        }
+    }
+}
