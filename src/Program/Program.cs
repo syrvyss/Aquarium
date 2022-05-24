df14fc9de64c89165a1b60086685c67d6b54b081
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-24 12:52:14 +0200

    Added AquariumCalculator and Volume method

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Aquarium {
+﻿using System;
+
+namespace Aquarium {
     class Program {
         public static void Main() {
             
+        }
+    }
+
+    class AquariumCalculator {
+        static double CalculateVolume(int length, int height) {
+            Math.Pow((double)length * (double)height, 3) / 1000;
         }
     }
 }
